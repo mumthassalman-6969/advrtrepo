d4fcb491f35a01c71507ebca94c7613be1c88f25
mumthassalman-6969 <mumthasvk19@gmail.com>
2021-12-04 23:24:29 +0530

    addmiddleware

--- advrtproject/settings.py
+++ advrtproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sd%3-jus(c=8z9mnwt+lb^hpxs6u)f5$inz_uxyw^uyt=a7pi-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
